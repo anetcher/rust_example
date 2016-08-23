f8a35a952ca54d844b6c88c9f67948e500054246
anetcher <aneutral@hotmail.com>
2016-08-24 00:09:58 +0900

    Add option match

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
     println!("Hello, rust!");
+
+    for argument in args {
+        match argument.as_ref() {
+            "1" => println!("One!"),
+            _ => println!("Other!")
+        }
+    }
 }
